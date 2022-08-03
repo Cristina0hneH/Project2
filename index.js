546ac46b5a13ea4cd7d5ef8ff69ecd1cf9e91438
Ibraheem Amer <iamer0237@gmail.com>
2022-08-03 16:40:36 +0200

    html file created, js and css filed modified

--- index.js
+++ index.js
@@ -36,3 +36,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
   
     // Your code to run since DOM is loaded and ready
   });
+  //Display image titel
+  function showText(recipeTitle) {
+    document.getElementById("imageTitel").innerHTML = recipeTitle;
+}
+function hide() {
+    document.getElementById("imageTitel").innerHTML = "";
+}
+//collaps
+function collaps() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
